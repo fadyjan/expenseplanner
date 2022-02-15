6ce11f7878afa515fa3f659c1f3732704dcd4d5a
fady jan <fady.ashraf@spimesenselabs.com>
2022-02-16 00:47:20 +0200

    Create Transcations Class

---
+++ lib/transaction.dart
@@ -0,0 +1,11 @@
+class Transaction {
+  final String id;
+  final String tittle;
+  final DateTime date;
+  final double amount;
+  Transaction(
+      {required this.id,
+      required this.tittle,
+      required this.date,
+      required this.amount});
+}
