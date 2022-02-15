dbfe9d5a3d8c8dbe5846bfdbf52b8e11967f3418
fady jan <fady.ashraf@spimesenselabs.com>
2022-02-16 00:51:53 +0200

    Create The Main Design of the main Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:expenseplanner/transaction.dart';
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,15 +15,69 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transcations = [
+    Transaction(id: "t1", tittle: "newshoes", date: DateTime.now(), amount: 69),
+    Transaction(
+        id: "t2", tittle: "newshoes3", date: DateTime.now(), amount: 69),
+    Transaction(
+        id: "t3", tittle: "newshoes2", date: DateTime.now(), amount: 69),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Flutter App'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: const Card(
+                child: const Text("This is FirstCard"),
+                color: Colors.blue,
+              ),
+            ),
+            Column(
+              children: transcations.map((tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                          margin: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration:
+                              BoxDecoration(border: Border.all(width: 2)),
+                          padding: const EdgeInsets.all(8),
+                          child: Text(
+                            tx.amount.toString(),
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.tittle,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 10),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
